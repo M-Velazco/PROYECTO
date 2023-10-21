322b701fbcbba6987bd7ba887baf7df71e3f6b91
JuanDavid580 <131486083+JuanDavid580@users.noreply.github.com>
2023-10-20 19:06:58 -0500

    Update Curso.php

--- Curso/Clases/Curso.php
+++ Curso/Clases/Curso.php
@@ -16,14 +16,18 @@
     // Metodos
 
     public function __construct(){
-       $this->con = new conexion  
-
-
+       $this->con = new conexion (); 
     }
 
+    public function set($atributo, $contenido){
+      $this->$atributo = $contenido;
+    } 
+
+    public function get($atributo){
+        return $this->$atributo;     
+    }
+
+    public function crear ()
+
    }
-
-
-
-
-?>
+   ?>
