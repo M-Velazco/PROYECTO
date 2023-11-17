7b588b532dce93849b671971df1c314ab66a6af7
Magdy v.v <131469830+M-Velazco@users.noreply.github.com>
2023-11-17 17:30:29 -0500

    .

--- Proyecto/src/app/app-routing.module.ts
+++ Proyecto/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/form', pathMatch: 'full' },
 ];
 
 @NgModule({
