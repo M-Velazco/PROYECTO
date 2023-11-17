8da3b64a80982d6c6bd5bfd2b073fa6e0329643e
Magdy v.v <131469830+M-Velazco@users.noreply.github.com>
2023-11-17 16:45:16 -0500

    .

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -2,26 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { FormComponent } from './form/form.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component'; // Remove the ".ts" extension
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     FormComponent,
-    LoginComponent,
-    RegistrationComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ChatComponent,
-    FormComponent
+    FormComponent,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
