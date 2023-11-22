ca69afe6cda6fd6360759a529d4f182a0f9adc6f
STIVENDJ <oliverossilvajohan@gmail.com>
2023-11-21 20:57:01 -0500

    s
    cambie la connexion que me salia un error

--- API PHP/conexion.php
+++ API PHP/conexion.php
@@ -1,6 +1,5 @@
 <?php
 
-// DATOS DE CONEXION A LA BASE DE DATOS
 function conexion() {
   $conexion = mysqli_connect("localhost", "root", "", "");
   
--- Angular/usuarios.service.ts
+++ Angular/usuarios.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UsuariosService {
 
-  URL = "http://localhost/api/";
+  URL = "";
 
   constructor(private http: HttpClient) { }
 
