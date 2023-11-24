e070a3bd3d827587a1ad60c4115ac5765f1c94d9
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2023-11-24 14:51:09 -0500

    Finalizacion de crud  Materias
    esta es la ultima actualizacion backend de la crud
    
    Nota: se evidencia que en modificacion.php se requiere el uso de comillas simples al pasar un $params de caracter string
    
    las siguientes actualizaciones seran de css y bootstrap

--- Materias/src/app/php/modificacion.php
+++ Materias/src/app/php/modificacion.php
@@ -3,16 +3,17 @@
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
   
   $json = file_get_contents('php://input');
- 
   $params = json_decode($json);
   
   require("conexion.php");
-  $con=retornarConexion();
-  
+  $con = retornarConexion();
 
-    mysqli_query($con,"update materias set nom_materia=$params->nom_materia where idmaterias=$params->idmaterias");
-    
-  
+  // Utiliza mysqli_real_escape_string para escapar el valor de nom_materia
+  $nom_materia = mysqli_real_escape_string($con, $params->nom_materia);
+
+  // Actualiza la consulta SQL utilizando comillas para el valor de nom_materia
+  mysqli_query($con, "update materias set nom_materia='$nom_materia' where idmaterias=$params->idmaterias");
+
   class Result {}
 
   $response = new Result();
