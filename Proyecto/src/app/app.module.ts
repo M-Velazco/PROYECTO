89425b66446a6671d7d2a9bc9ffa7933140f94b9
Magdy Velazco <mvelazcovelasco17@gmail.com>
2023-11-24 14:37:20 -0500

    24/11/23

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './Login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     LoginComponent,
+    PrincipalComponent,
 
   ],
   imports: [
