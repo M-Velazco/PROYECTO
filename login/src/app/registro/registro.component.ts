e9ae50bd17c1a945c29084bb7f97fc060e8644b5
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2023-11-24 17:23:37 -0500

    update register
    Co-Authored-By: JuanDavid580 <131486083+JuanDavid580@users.noreply.github.com>

--- login/src/app/registro/registro.component.ts
+++ login/src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegistroService } from './registro.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  usuarios: any;
 
+  user={
+    Idusuarios:0,
+    Nombre_apellido:"",
+    Correo:"",
+    Telefono:0,
+    Contraseña:"",
+    Rol:""
+  }
+
+  constructor(private registroservicio:RegistroService){}
 }
