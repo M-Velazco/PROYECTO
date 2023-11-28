690ab4f61b06aa6e8b43d473d26433c23ddd020b
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2023-11-28 09:26:42 -0500

    Update app.module.ts
    cambios realizados a la crud de padres de familia

--- crud_padres_de_familia/App/src/app/app.module.ts
+++ crud_padres_de_familia/App/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-
-import {HttpClientModule} from '@angular/common/http';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppModule { }
+export class AppComponent implements OnInit {
+ data: any;
+
+ constructor(private http: HttpClient) { }
+
+ ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe(response => {
+      console.log(response);
+      this.data = response;
+    }, error => {
+      console.log(error);
+    });
+ }
+}
