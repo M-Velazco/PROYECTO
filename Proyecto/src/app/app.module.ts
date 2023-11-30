056f1a728589d263d886b784288c790652266118
Magdy Velazco <mvelazcovelasco17@gmail.com>
2023-11-30 17:33:05 -0500

    Actualización de login
    Actualización de login

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -2,27 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms'; // Agrega esta línea
-
 import { AppComponent } from './app.component';
-import { ChatComponent } from './chat/chat.component';
-import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
-import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent,
-    InicioSesionComponent,
-    RegistroComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule, // Agrega esta línea
+    HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
--- Proyecto/src/app/chat/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-function retornarConexion() {
-  $con=mysqli_connect("127.0.0.1","root","","chat");
-  return $con;
-}
-?>
