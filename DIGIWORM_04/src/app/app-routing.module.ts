96ff82d33c13b2bf66d033f565f2cd64d9853959
Magdy Velazco <mvelazcovelasco17@gmail.com>
2023-12-07 16:36:54 -0500

    Co-authored-by: j-s-roa <j-s-roa@users.noreply.github.com>

--- DIGIWORM_04/src/app/app-routing.module.ts
+++ DIGIWORM_04/src/app/app-routing.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
- 
-  { path: 'login', component: LoginComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: '**'
-  
-  , pathMatch: 'full', redirectTo: 'registloginro' },
-  
-  ];
+  { path: 'Index', component: IndexComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: '', redirectTo: 'Index', pathMatch: 'full' },
+   // Ruta por defecto para manejar rutas no encontradas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
