94e792d07c1465fb7626e21d18367bcd22357972
Magdy Velazco <mvelazcovelasco17@gmail.com>
2023-12-07 15:22:54 -0500

    index

--- DIGIWORM_04/src/app/app.module.ts
+++ DIGIWORM_04/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { LoginComponent } from './components/login/login.component';
 
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
