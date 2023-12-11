58ffae9471442e3509d8176f1b4d659b441ce42b
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2023-12-11 06:27:17 -0500

    Update login.php
    Prueba php login

--- DIGIWORM_04/src/app/components/login/php/login.php
+++ DIGIWORM_04/src/app/components/login/php/login.php
@@ -1,44 +1,18 @@
-<?php
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  require("conexion.php");
+  $con=retornarConexion();
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-
-require('conexion.php');
-$con = retornarConexion();
-
-// Obtener datos del método GET
-$idusuarios = $_GET['idusuarios'] ?? null;
-$contrasena = $_GET['Contrasena'] ?? null;
-
-// Verificar si se proporcionaron los datos
-if ($idusuarios !== null && $contrasena !== null) {
-    $query = "SELECT idusuarios, Contrasena FROM usuarios WHERE idusuarios = '$idusuarios' AND Contrasena = '$contrasena'";
+  $registros=mysqli_query($con,"select idusuarios, Contrasena from usuarios where idusuarios=$_GET[idusuarios] and Contrasena=$_GET[Contrasena]");
     
-    // Ejecutar la consulta
-    $result = mysqli_query($con, $query);
-
-    if ($result) {
-        // Verificar si hay resultados
-        if (mysqli_num_rows($result) > 0) {
-            // Obtener resultados como un array asociativo
-            $vec = mysqli_fetch_assoc($result);
-
-            // Enviar respuesta JSON
-            echo json_encode(['success' => true, 'data' => $vec]);
-        } else {
-            // No hay resultados
-            echo json_encode(['success' => false, 'error' => 'No se encontraron resultados']);
-        }
-    } else {
-        // Error en la consulta
-        echo json_encode(['success' => false, 'error' => 'Error en la consulta']);
-    }
-} else {
-    // Datos no proporcionados
-    echo json_encode(['success' => false, 'error' => 'Datos no proporcionados']);
-}
-
-// Cerrar la conexión
-mysqli_close($con);
-
+  if ($reg=mysqli_fetch_array($registros))  
+  {
+    $vec[]=$reg;
+  }
+  
+  $cad=json_encode($vec);
+  echo $cad;
+  header('Content-Type: application/json');
 ?>
