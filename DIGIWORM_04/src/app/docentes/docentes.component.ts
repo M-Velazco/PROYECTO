2d37a680148630e734d22a150b8c83c79d9d37bc
M-Velazco <mvelazcovelasco17@gmail.com>
2023-12-11 18:42:46 -0500

    cambios
    Co-Authored-By: STIVENDJ <131485773+STIVENDJ@users.noreply.github.com>
    Co-Authored-By: j-s-roa <131488423+j-s-roa@users.noreply.github.com>

--- DIGIWORM_04/src/app/docentes/docentes.component.ts
+++ DIGIWORM_04/src/app/docentes/docentes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticulosService } from './articulos.service';
 
-
 @Component({
   selector: 'app-docentes',
   templateUrl: './docentes.component.html',
