197468675bf56f9e976c5721137ed1a6b57fb05e
STIVENDJ <mvelazcovelasco17@gmail.com>
2023-12-11 09:06:16 -0500

    .

--- DIGIWORM_04/src/app/app-routing.module.ts
+++ DIGIWORM_04/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { IndexComponent } from './index/index.component';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
   { path: 'Index', component: IndexComponent },
   { path: 'Login', component: LoginComponent },
+  {path:'chat',component:ChatComponent},
   { path: '', redirectTo: 'Index', pathMatch: 'full' },
+
    // Ruta por defecto para manejar rutas no encontradas
 ];
 
