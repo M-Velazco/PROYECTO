d7d995ac2f1e505e93d8a364667bba745521857f
LOMBANAA24 <mvelazcovelasco17@gmail.com>
2024-02-20 15:54:39 -0500

    login

--- DIGIWORM/chat/chat.php
+++ DIGIWORM/chat/chat.php
@@ -13,14 +13,14 @@ if (!isset($_SESSION['unique_id'])) {
       <header>
         <?php
         $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+        $sql = mysqli_query($conn, "SELECT * FROM usuarios WHERE unique_id = {$user_id}");
         if (mysqli_num_rows($sql) > 0) {
           $row = mysqli_fetch_assoc($sql);
         } else {
-          header("location: users.php");
+          header("location: usuarios.php");
         }
         ?>
-        <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+        <a href="usuarios.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
         <img src="php/images/<?php echo $row['img']; ?>" alt="">
         <div class="details">
           <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
