97abb7434cfcb047a69a2f2a3e9af2b52f3f8d88
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2024-02-22 15:36:21 -0500

    Update config.php
    cambio mayuscula

--- DIGIWORM/chat/php/config.php
+++ DIGIWORM/chat/php/config.php
@@ -1,10 +1,10 @@
 <?php
 $hostname = "localhost";
 $username = "root";
-$password = "sena";
+$password = "";
 $dbname = "digiworm_04";
 
-$conn = mysqli_connect($hostname, $username, $Password, $dbname);
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
   echo "Database connection error" . mysqli_connect_error();
 }
