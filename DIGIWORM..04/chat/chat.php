8ecfc3d462ab447a417bbb83f0144f391c2ebfda
M-Velazco <mvelazcovelasco17@gmail.com>
2024-02-23 19:40:09 -0500

    Cambios
    Se realizaron Visuales con cambios

---
+++ DIGIWORM..04/chat/chat.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include_once "php/config.php";
+if (!isset($_SESSION['Idusuarios'])) {
+  header("location: login.php");
+  exit; // Asegúrate de salir del script después de redirigir
+}
+
+include_once "header.php";
+?>
+
+<body>
+  <div class="wrapper">
+    <section class="chat-area">
+      <header>
+        <?php
+        if (isset($_GET['Idusuarios'])) {
+          $user_id = mysqli_real_escape_string($conn, $_GET['Idusuarios']);
+          $sql = mysqli_query($conn, "SELECT * FROM usuarios WHERE Idusuarios = {$user_id}");
+          if ($sql && mysqli_num_rows($sql) > 0) {
+            $row = mysqli_fetch_assoc($sql);
+          } else {
+            header("location: users.php");
+            exit; // Asegúrate de salir del script después de redirigir
+          }
+        } else {
+          // Maneja el caso en que $_GET['Idusuarios'] no está definido
+          echo "ID de usuario no definido";
+          exit; // Asegúrate de salir del script si no hay ID de usuario
+        }
+        ?>
+        <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+        <img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <div class="details">
+          <span><?php echo $row['Nombres'] . " " . $row['Apellidos'] ?></span>
+          <p><?php echo $row['status']; ?></p>
+        </div>
+      </header>
+      <div class="chat-box">
+
+      </div>
+      <form action="#" class="typing-area">
+        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+        <input type="text" name="mensajes" class="input-field" placeholder="Escribe tu mensaje aquí..." autocomplete="off">
+        <button class="button" name="button"><i class="fab fa-telegram-plane"></i></button>
+      </form>
+    </section>
+  </div>
+
+  <script src="javascript/chat.js"></script>
+
+</body>
+
+</html>
---
+++ DIGIWORM..04/chat/php/login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include_once "config.php";
+$Email = mysqli_real_escape_string($conn, $_POST['Email']);
+$Password = mysqli_real_escape_string($conn, $_POST['Pasword']);
+if (!empty($Email) && !empty($Password)) {
+    $sql = mysqli_query($conn, "SELECT * FROM usuarios WHERE Email = '{$Email}'");
+    if (mysqli_num_rows($sql) > 0) {
+        $row = mysqli_fetch_assoc($sql);
+        $user_pass = md5($Password);
+        $enc_pass = $row['Pasword'];
+        if ($user_pass === $enc_pass) {
+            $status = "Disponible";
+            $sql2 = mysqli_query($conn, "UPDATE usuarios SET status = '{$status}' WHERE Idusuarios = {$row['Idusuarios']}");
+            if ($sql2) {
+                $_SESSION['Idusuarios'] = $row['Idusuarios'];
+                echo "Proceso Exitoso";
+            } else {
+                echo "Algo salió mal. ¡Inténtalo de nuevo!";
+            }
+        } else {
+            echo "¡Correo electrónico o la contraseña son incorrectos!";
+        }
+    } else {
+        echo "$Email - ¡Este correo electrónico no existe!";
+    }
+} else {
+    echo "¡Todos los campos de entrada son obligatorios!";
+}
