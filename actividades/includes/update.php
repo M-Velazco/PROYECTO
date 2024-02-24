34c82fd596c60dcf9ccd4cc407e93ba7bb9024b7
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2024-02-23 19:41:35 -0500

    avance actividades
    avances en actividades

--- actividades/includes/update.php
+++ actividades/includes/update.php
@@ -3,7 +3,8 @@ if (isset($_POST['registrar'])) {
     $idactividades = $_GET['idActividades']; // Obtén el ID del registro a actualizar
     $nombreA = $_POST['nombreA'];
     $Materia = $_POST['Materia'];
-
+    $docente = $_POST['Docente'];
+    $estado = $_POST['Estado'];
     $carpeta_destino = "files/";
 
     // Obtener el nombre y la extensión del archivo
@@ -13,7 +14,7 @@ if (isset($_POST['registrar'])) {
     if (move_uploaded_file($_FILES["Archivo"]["tmp_name"], $carpeta_destino . $nombre_archivo)) {
         require_once "../includes/db.php"; // Asegúrate de incluir la conexión a la base de datos
 
-        $sql = "UPDATE actividades SET Nom_act = '$nombreA', Materia_act = '$Materia', Archivo = '$nombre_archivo' WHERE idactividades = $idactividades";
+        $sql = "UPDATE actividades SET Nombre_act = '$nombreA', Asignatura = '$Materia', Docente = '$docente', Archivo = '$nombre_archivo', Estado = '$estado' WHERE idActividades = $idactividades";
         $resultado = mysqli_query($conexion, $sql);
 
         if ($resultado) {
