cbb0eab94408bf74b4ada0f3b59940351e835e26
Magdy v.v <131469830+M-Velazco@users.noreply.github.com>
2024-03-01 14:23:29 -0500

    Delete DIGIWORM_04 directory
    Se elimina por motivos de cambios

--- DIGIWORM_04/src/app/curso/Backend/Curso/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-function retornarConexion() {
-  $con = mysqli_connect("localhost", "root", "", "digiworm");
-  if (!$con) {
-    die("Error de conexión: " . mysqli_connect_error());
-  }
-  return $con;
-}
-?>
