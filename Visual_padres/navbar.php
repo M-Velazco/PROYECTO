e38c4aaec21cc70b309f4b710889dfc52d08fc9b
JuanDavid580 <131486083+JuanDavid580@users.noreply.github.com>
2024-03-05 15:10:03 -0500

    Delete Visual_padres directory

--- Visual_padres/navbar.php
+++
@@ -1,6 +0,0 @@
-<div class="search-bar">
-    <form method="post" action="buscar_estudiante.php">
-        <input type="text" name="idEstudiante" placeholder="Buscar por ID (solo números)" pattern="[0-9]+" required>
-        <button type="submit">Buscar</button>
-    </form>
-</div>
