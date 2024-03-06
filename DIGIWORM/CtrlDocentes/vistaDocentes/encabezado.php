3ce3a0e4670588c9e8d12d5ac169e2d7cad4b168
M-Velazco <mvelazcovelasco17@gmail.com>
2024-03-06 14:47:33 -0500

    Eliminacion
    Se realizo la eliminación de una carpeta

--- DIGIWORM/CtrlDocentes/vistaDocentes/encabezado.php
+++
@@ -1,5 +0,0 @@
-<table width="70%" border="0" align="center">
-  <tr>
-    <td align="center"><H1>*+* DOCENTES *+*</H1></td>
-  </tr>
-</table>
--- DIGIWORM/CtrlDocentes/vistaDocentes/piePagina.php
+++
@@ -1,5 +0,0 @@
-<table width="70%" border="0" align="center">
-  <tr>
-    <td><strong></strong>-- “La vida es como el jazz… es mucho mejor si es improvisada.” George Gershwin --<strong></strong>“La vida es como el jazz… es mucho mejor si es improvisada.” George Gershwin </td>
-  </tr>
-</table>
