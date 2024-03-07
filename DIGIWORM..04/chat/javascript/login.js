e750b0c0b62aeab8c400af0eb1aac48cb1564076
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2024-03-07 10:51:53 -0500

    Update login.js
    Redireccion funcional

--- DIGIWORM..04/chat/javascript/login.js
+++ DIGIWORM..04/chat/javascript/login.js
@@ -13,9 +13,9 @@ continueBtn.onclick = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         let data = xhr.responseText;
-        if (data.trim() === "success") {
-          // Redirige al usuario a la misma página para recargarla después de un éxito
-          window.location.reload();
+        if (data.trim() === "Proceso Exitoso") { // Modificado para coincidir con la respuesta PHP
+          // Redirige al usuario a users.php después de un inicio de sesión exitoso
+          window.location.href = "users.php";
         } else {
           errorText.style.display = "block";
           errorText.textContent = data;
@@ -24,5 +24,6 @@ continueBtn.onclick = () => {
     }
   }
   let formData = new FormData(form);
+  // Envía el formulario
   xhr.send(formData);
 }
