e460f5ab769e9123f7d043a6f36bd4fe44c50cdb
j-s-roa <131488423+j-s-roa@users.noreply.github.com>
2024-03-09 22:32:26 -0500

    Funcional de Actividades
    Funcional de actividades y añadir campo de descripcion en la tabla de Actividades

--- DIGIWORM..04/Actividades.php
+++ DIGIWORM..04/Actividades.php
@@ -137,143 +137,70 @@ endif;
     <!-- Header End -->
     <!-- Blog Start -->
     <div class="container-fluid pt-5">
-        <div class="container">
-            <div class="text-center pb-2">
-                <p class="section-title px-5"><span class="px-2">Propuestas</span></p>
-                <h1 class="mb-4">Actividades</h1>
-            </div>
-            <?php 
+    <div class="container">
+        <div class="text-center pb-2">
+            <p class="section-title px-5"><span class="px-2">Propuestas</span></p>
+            <h1 class="mb-4">Actividades</h1>
+        </div>
+        
+        <div class="row pb-3">
+            <?php
             $conexion = new mysqli("localhost", "root", "sena", "digiworm_04");
             if ($conexion->connect_error) {
                 die("Error de conexión: " . $conexion->connect_error);
             }
             $consulta = $conexion->query("SELECT  * FROM actividades");
 
-            // Generar las opciones del select
-            // while ($fila = $consulta->fetch_assoc()) {?>
-
-            <div class="row pb-3">
+            while ($fila = $consulta->fetch_assoc()) {
+                // Obtener el nombre del docente
+                $consulta_docente = $conexion->query("SELECT Nombres FROM docente WHERE idDocente = {$fila['Docente']}");
+                $docente = $consulta_docente->fetch_assoc();
+            ?>
                 <div class="col-lg-4 mb-4">
                     <div class="card border-0 shadow-sm mb-2">
                         <img class="card-img-top mb-2" src="img/blog-1.jpg" alt="">
                         <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Quimica</h4>
+                            <h4 class=""><?php echo $fila['Nombre_act']; ?></h4>
+                            
                             <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
+                                <small class="mr-3"><i class="fa fa-user text-primary"></i>  <?php echo $docente['Nombres']; ?></small>
+                                <small class="mr-3"><i class="fas fa-book" style="color: #38ee2b;"></i><?php echo $fila['Asignatura']; ?></small>
+                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> <?php echo $fila['Archivo']; ?></small>
+                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> <?php echo $fila['Estado']; ?></small>
                             </div>
-                            <p>Habilidades de investigación sólidas
-                                Analítico y con visión de futuro</p>
-                                <p>Buena solución de problemas</p>
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card border-0 shadow-sm mb-2">
-                        <img class="card-img-top mb-2" src="img/blog-2.jpg" alt="">
-                        <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Fisica</h4>
-                            <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
-                            </div>
-                            <p>Investigacion sobre que es la fisica </p>
-                            <p>De donde proviene la fisica</p>
-                            <p>Quien invento la fisica</p>
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card border-0 shadow-sm mb-2">
-                        <img class="card-img-top mb-2" src="img/blog-3.jpg" alt="">
-                        <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Español</h4>
-                            <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
-                            </div>
-                            <p>S <p>Investigar las tendencias del sector</p>
-                              <p> Desarrollar estrategias de marca</p> 
 
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
+                            <p><?php echo $fila['Descripcion']; ?></p>
+                            <a href="includes/download.php?idActividades= <?php echo $fila['idActividades'] ;?>" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card border-0 shadow-sm mb-2">
-                        <img class="card-img-top mb-2" src="img/blog-1.jpg" alt="">
-                        <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Filosofia</h4>
-                            <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
-                            </div>
-                            <p>Si tu empleador acepta ajustar tu función a la descripción que crees, puede que te encuentres con un trabajo gratificante y agradable hecho a tu medida.</p>
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card border-0 shadow-sm mb-2">
-                        <img class="card-img-top mb-2" src="img/blog-2.jpg" alt="">
-                        <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Religion</h4>
-                            <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
-                            </div>
-                            <p>En lugar de esperar a encontrar el puesto de trabajo perfecto o a que tu empleador te ascienda a un puesto mejor, pasa a la acción creando una nueva descripción del puesto y presentándola a tu jefe. Las ventajas de redactar tu propia descripción del puesto son, entre otras, las siguientes:</p>
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card border-0 shadow-sm mb-2">
-                        <img class="card-img-top mb-2" src="img/blog-3.jpg" alt="">
-                        <div class="card-body bg-light text-center p-4">
-                            <h4 class="">Actividad Danzas</h4>
-                            <div class="d-flex justify-content-center mb-3">
-                                <small class="mr-3"><i class="fa fa-user text-primary"></i> Docente</small>
-                                <small class="mr-3"><i class="fa fa-folder text-primary"></i> Materia</small>
-                                <small class="mr-3"><i class="fa fa-comments text-primary"></i> Entregados</small>
-                            </div>
-                            <p>Crear el trabajo perfecto para ti y para tu empleador es posible si escribes tu propia descripción del trabajo. Es posible que quieras escribir tu propia descripción del trabajo por las siguientes razones:</p>
-                            <a href="" class="btn btn-primary px-4 mx-auto my-2">Publicar</a>
-                        </div>
-                    </div>
-                </div>
-                <>?
-                <div class="col-md-12 mb-4">
-                    <nav aria-label="Page navigation">
-                      <ul class="pagination justify-content-center mb-0">
-                        <li class="page-item disabled">
-                          <a class="page-link" href="#" aria-label="Previous">
+            <?php } ?>
+        </div>
+        <div class="col-md-12 mb-4">
+            <nav aria-label="Page navigation">
+                <ul class="pagination justify-content-center mb-0">
+                    <li class="page-item disabled">
+                        <a class="page-link" href="#" aria-label="Previous">
                             <span aria-hidden="true">&laquo;</span>
                             <span class="sr-only">Previous</span>
-                          </a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                          <a class="page-link" href="#" aria-label="Next">
+                        </a>
+                    </li>
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
                             <span aria-hidden="true">&raquo;</span>
                             <span class="sr-only">Next</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                </div>
-            </div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
+</div>
+
+
     <!-- Blog End -->
     <!-- Footer Start -->
     <div class="container-fluid bg-secondary text-white mt-5 py-5 px-sm-3 px-md-5">
