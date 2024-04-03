8349935a8a0a13c4abc9e376bbd87b9762877e83
M-Velazco <mvelazcovelasco17@gmail.com>
2024-04-03 15:52:55 -0500

    .

---
+++ DIGIWORM..04/cursos/tabla_datos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla con CSS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <table class="styled-table">
+        <thead>
+            <tr>
+                <th>Id Estudainte</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Email</th>
+                <th>Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Carlos</td>
+                <td>25</td>
+                <td>Madrid</td>
+                <td>Madrid</td>
+                <td>Madrid</td>
+            </tr>
+            <tr>
+                <td>Ana</td>
+                <td>30</td>
+                <td>Barcelona</td>
+                <td>Barcelona</td>
+                <td>Barcelona</td>
+            </tr>
+            <tr>
+                <td>Pablo</td>
+                <td>28</td>
+                <td>Sevilla</td>
+                <td>Sevilla</td>
+                <td>Sevilla</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
