f6674f1a6f0cdb869969e29598329384422a17b4
Magdy v.v <mvelazcovelasco17@gmail.com>
2024-06-26 21:31:30 -0500

    .

--- DIGIWORM..04/flutter_application_1/lib/Interfaz/index.js
+++ DIGIWORM..04/flutter_application_1/lib/Interfaz/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
-const port = 3306; // Puerto en el que se ejecutará el servidor
+const port = 3307; // Puerto en el que se ejecutará el servidor
+
+// Configura el proxy para redirigir solicitudes a form.php
+app.use('/form.php', createProxyMiddleware({
+  target: 'http://localhost/PROYECTO/DIGIWORM..04',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/form.php': '/form.php', // Reescribe la ruta para que coincida con la ruta del backend PHP
+  },
+}));
 
 // Rutas de ejemplo
 app.get('/', (req, res) => {
